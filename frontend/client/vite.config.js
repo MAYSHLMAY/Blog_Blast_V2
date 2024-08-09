fd6ec97f6dda3004db914e4fe92cd6f33df544f4
Mike <educationforlife424@gmail.com>
2024-08-09 12:13:35 +0300

    done

--- frontend/client/vite.config.js
+++ frontend/client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'blog-blast-v2.vercel.app',
         secure: false,
       },
     },
