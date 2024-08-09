08a3f7858305306d202c172289ebb086560c079e
Mike <educationforlife424@gmail.com>
2024-08-09 16:57:46 +0300

    done

--- frontend/client/vite.config.js
+++ frontend/client/vite.config.js
@@ -11,7 +11,6 @@ export default defineConfig({
       '/api': {
         target: 'https://blog-blast-v2-backend.onrender.com',
         secure: false,
-        changeOrigin: false,
       },
     },
   },
