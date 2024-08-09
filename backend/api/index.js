7cf1c851c1e7e334f04f08e687c21ac8d60acfb8
Mike <educationforlife424@gmail.com>
2024-08-09 14:46:36 +0300

    not done

--- backend/api/index.js
+++ backend/api/index.js
@@ -20,15 +20,15 @@ mongoose
 
 const __dirname = path.resolve();
 const app = express();
-
-
-app.use(express.json());
-app.use(cookieParser());
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true
 }));
+
+app.use(express.json());
+app.use(cookieParser());
+
 
 // Simple route to test server is up and running
 app.get('/', (req, res) => {
