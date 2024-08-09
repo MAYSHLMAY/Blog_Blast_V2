272dfbe0a1a91252ae8feac498413a9c1239a458
Mike <educationforlife424@gmail.com>
2024-08-09 15:55:56 +0300

    done

--- frontend/client/vite.config.js
+++ frontend/client/vite.config.js
@@ -1,15 +1,17 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'client/dist', // Output directory for production build
+  },
   server: {
     proxy: {
       '/api': {
         target: 'https://blog-blast-v2-backend.onrender.com',
-        secure: false, // Use true if HTTPS with a valid certificate
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false,
+        changeOrigin: false, // Helps with CORS issues during development
       },
     },
   },
